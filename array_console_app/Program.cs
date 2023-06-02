d9d7000a0609be30d615347e4a31a922194def48
aeman <aeman@MANGTAY>
2023-06-02 23:03:13 +0300

    Add project files.

---
+++ array_console_app/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array_console_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number :");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int[] array = new int[number + 1];
+            array[0] = 0;
+            array[1] = 1;
+            for (int i = 2; i < number; i++)
+            {
+                array[i] = array[i - 1] + array[i - 2];
+
+            }
+            Console.WriteLine("Fibonacci calculation up to the entered value");
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
